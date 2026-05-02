8c93a0bb2d603248132eb010889d813e4da2b285
ChauhanAntima <87909369+ChauhanAntima@users.noreply.github.com>
2026-05-02 19:48:14 +0530

    app.ts and server.ts setup

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+import app from './src/app.js';
+
+const StartServer = () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+
+}
+);
+}
+StartServer();
